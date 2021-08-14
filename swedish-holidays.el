5bfad65f72fe806ba404ddc666b5abe287c853bb
Björn Larsson <b.e.larsson@gmail.com>
2021-08-14 22:36:56 +0200

    Add package file

---
+++ swedish-holidays.el
@@ -0,0 +1,40 @@
+;;; swedish-holidays.el --- Provides Swedish holidays for the calendar -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Björn Larsson
+;;
+;; Author: Björn Larsson <https://github.com/fuzzycode>
+;; Maintainer: Björn Larsson <develop@bjornlarsson.net>
+;; Created: August 14, 2021
+;; Modified: August 14, 2021
+;; Version: 0.0.1
+;; Keywords: calendar swedish localization
+;; Homepage: https://github.com/bjornlarsson/swedish-holidays
+;; Package-Requires: ((emacs "24.4"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;;  Provides Swedish holidays for Emacs calendar
+;;
+
+
+;;; Code:
+
+
+
+(provide 'swedish-holidays)
+;;; swedish-holidays.el ends here
